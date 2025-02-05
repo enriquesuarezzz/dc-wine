33d1a1ec7f2438f3e7949950b212e2ce8020478c
enriquesuarezzz <enriquesuarezmartin@gmail.com>
2025-02-05 15:45:40 +0000

    PageErrorFixed

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,7 +2,7 @@ import { Link } from '@/i18n/routing'
 import { getTranslations } from 'next-intl/server'
 
 export default async function HomePage() {
-  const t = await getTranslations('HomePage')
+  const t = await getTranslations('home_page')
   return (
     <div>
       <h1>{t('title')}</h1>
