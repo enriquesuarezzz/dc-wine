9fbab0f41fabb2ae3da9fc4cac18b2210cc2bc96
enriquesuarezzz <enriquesuarezmartin@gmail.com>
2025-02-06 21:27:55 +0000

    PageUpdate

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -5,8 +5,7 @@ export default async function HomePage() {
   const t = await getTranslations('home_page')
   return (
     <div>
-      <h1>{t('title')}</h1>
-      <Link href="/contact">{t('contact')}</Link>
+     
     </div>
   )
 }
