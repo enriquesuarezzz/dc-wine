d0e99fef07ab34042e0ccb0185fb981feace8b9c
enriquesuarezzz <enriquesuarezmartin@gmail.com>
2025-02-06 21:28:25 +0000

    localeSwitcherSelectUpdate

--- src/components/molecules/locale_switcher_select/locale_switcher_select.tsx
+++ src/components/molecules/locale_switcher_select/locale_switcher_select.tsx
@@ -2,7 +2,6 @@
 
 import { Locale, routing, usePathname, useRouter } from '@/i18n/routing'
 import { useParams } from 'next/navigation'
-import { ReactNode } from 'react'
 import {
   Select,
   SelectTrigger,
@@ -10,41 +9,51 @@ import {
   SelectContent,
   SelectItem,
 } from '../ui/select'
+import English from '@/components/atoms/svg/english'
+import Spanish from '@/components/atoms/svg/spanish'
+import { JSX } from 'react'
+
+const localeIcons: Record<string, JSX.Element> = {
+  es: <Spanish className="w-6 h-6" />,
+  en: <English className="w-6 h-6" />,
+}
+
+const localeNames: Record<string, string> = {
+  es: 'ES',
+  en: 'EN',
+}
 
 type Props = {
-  children: ReactNode
   defaultValue: string
   label: string
 }
 
 export default function LocaleSwitcherSelect({ defaultValue, label }: Props) {
   const router = useRouter()
-
   const pathname = usePathname()
   const params = useParams()
 
   function onSelectChange(nextLocale: string) {
     router.replace(
-      // @ts-expect-error -- TypeScript will validate that only known `params`
-      // are used in combination with a given `pathname`. Since the two will
-      // always match for the current route, we can skip runtime checks.
-      { pathname, params },
-      { locale: nextLocale as Locale },
+      { pathname },
+      { locale: nextLocale as Locale }
     )
   }
 
   return (
     <Select defaultValue={defaultValue} onValueChange={onSelectChange}>
-      <SelectTrigger
-        className="h-8 w-[80px] border-none bg-transparent focus:ring-0 focus:ring-offset-0"
-        aria-label={label}
-      >
-        <SelectValue />
+      <SelectTrigger className="bg-transparent">
+        <SelectValue >
+          {localeIcons[defaultValue]}
+        </SelectValue>
       </SelectTrigger>
-      <SelectContent>
+      <SelectContent className="bg-gray-200 flex rounded-lg border border-gray-200 p-2">
         {routing.locales.map((locale) => (
-          <SelectItem key={locale} value={locale}>
-            {locale.toUpperCase()}
+          <SelectItem key={locale} value={locale}    className="bg-gray flex p-2 rounded-md transition">
+             <div className="flex items-center gap-2">
+              {localeIcons[locale]} 
+              <span>{localeNames[locale]}</span>
+            </div>
           </SelectItem>
         ))}
       </SelectContent>
