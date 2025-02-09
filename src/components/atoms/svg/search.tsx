f57dd29d2b708dd71db3982ba45d8305e77177ee
enriquesuarezzz <enriquesuarezmartin@gmail.com>
2025-02-09 20:22:08 +0000

    SVGSColorsUpdate

--- src/components/atoms/svg/search.tsx
+++ src/components/atoms/svg/search.tsx
@@ -10,7 +10,7 @@ export default function Search(props: SVGProps<SVGSVGElement>) {
       {...props}
     >
       <path
-        fill="#737373"
+        fill="#000000"
         d="M15.096 5.904a6.5 6.5 0 1 0-9.192 9.192a6.5 6.5 0 0 0 9.192-9.192M4.49 4.49a8.5 8.5 0 0 1 12.686 11.272l5.345 5.345l-1.414 1.414l-5.345-5.345A8.501 8.501 0 0 1 4.49 4.49"
       />
     </svg>
