bb69cd44baf61fceea4eb2f882aa8ec66836c701
enriquesuarezzz <enriquesuarezmartin@gmail.com>
2025-02-10 14:36:55 +0000

    headerUpdate

--- src/components/molecules/header/header.tsx
+++ src/components/molecules/header/header.tsx
@@ -4,31 +4,31 @@ import { PoppinsText } from '@/components/atoms/poppins_text'
 export default async function Header() {
   const t = await getTranslations('home_page')
   return (
-    <div className="relative flex h-full w-full items-center justify-center">
+    <section className="relative flex h-full w-full items-center justify-center">
       <img
         src="images/header.avif"
         alt=""
         className="h-full max-h-[650px] w-full object-cover"
       />
       {/* Overlay Div */}
-      <div className="absolute right-72 rounded-lg bg-[#fff3e3] p-10">
+      <div className="bg-light_orange absolute right-72 rounded-lg p-10">
         <PoppinsText fontSize="16px" style="bold">
-          {t('header')}
+          {t('header.header_title')}
         </PoppinsText>
         <PoppinsText
-          className="pt-2 font-bold text-[#b98e2f]"
+          className="text-dark_orange pt-2 font-bold"
           fontSize="32px"
           style="bold"
         >
-          <span dangerouslySetInnerHTML={{ __html: t('title') }} />
+          <span dangerouslySetInnerHTML={{ __html: t('header.title') }} />
         </PoppinsText>
         <PoppinsText fontSize="16px" style="bold" className="pt-4">
-          {t('subtitle')}
+          {t('header.subtitle')}
         </PoppinsText>
-        <button className="mt-8 rounded-full bg-[#b98e2f] px-10 py-4 font-bold text-white">
-          {t('button')}
+        <button className="bg-darker_orange mt-8 rounded-full px-10 py-4 font-bold text-white">
+          {t('header.button')}
         </button>
       </div>
-    </div>
+    </section>
   )
 }
