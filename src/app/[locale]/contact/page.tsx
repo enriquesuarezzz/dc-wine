613d9fe3a9627acaf15799242c6dde3df043cbc9
enriquesuarezzz <enriquesuarezmartin@gmail.com>
2025-02-12 15:39:51 +0000

    AboutUsPageCreate

--- src/app/[locale]/contact/page.tsx
+++
@@ -1,9 +0,0 @@
-import { getTranslations } from 'next-intl/server'
-export default async function ContactPage() {
-  const t = await getTranslations('contact_page')
-  return (
-    <div>
-      <h1>{t('title')}</h1>
-    </div>
-  )
-}
