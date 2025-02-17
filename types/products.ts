69e00c9b44ec9a06c8edcb890ed7f59e34b1f015
enriquesuarezzz <enriquesuarezmartin@gmail.com>
2025-02-17 08:51:11 +0000

    typesProducts

---
+++ types/products.ts
@@ -0,0 +1,24 @@
+export interface Product {
+  id: string
+  name: string
+  category: string
+  price: number
+  grapes: string
+  origin: string
+  winemaking_technique: string
+  sparkling_wine_method: string
+  alcohol_content: string
+  residual_sugar: string
+  appearance: string
+  colour: string
+  bead: string
+  aroma: string
+  taste: string
+  storage: string
+  pairings: string
+  serving_temperature: string
+  size: string
+  packaging: string
+  imageUrl: string
+  stock: number
+}
