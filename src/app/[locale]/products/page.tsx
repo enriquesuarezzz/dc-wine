79dc39bee2c01279964b5286ac343c533e96fded
enriquesuarezzz <enriquesuarezmartin@gmail.com>
2025-02-17 14:43:57 +0000

    ProductsPage

--- src/app/[locale]/products/page.tsx
+++ src/app/[locale]/products/page.tsx
@@ -1,49 +1,25 @@
-import { Product } from '../../../../types/products'
-import { getProducts } from '../../../../lib/firestore'
 import { getTranslations } from 'next-intl/server'
 import { PoppinsText } from '@/components/atoms/poppins_text'
+import Products from '@/components/molecules/products/products'
 
-// Async page component to fetch data
-export default async function ProductsPage({
-  params,
-}: {
-  params: { locale: string }
-}) {
-  // Await the locale parameter
-  const locale = await params.locale
+export async function generateMetadata() {
+  const t = await getTranslations('products_page')
 
-  // Fetch data
-  const products: Product[] = await getProducts(locale)
-  const t = await getTranslations('products')
-
+  return {
+    title: t('meta_title'),
+    description: t('meta_description'),
+  }
+}
+export default async function ProductsPage() {
+  const t = await getTranslations('products_page')
   return (
     <div className="mx-20 flex flex-col justify-center pt-24">
+      {/* title */}
       <PoppinsText fontSize="56px" style="bold" className="max-w-[500px]">
         {t('title')}
       </PoppinsText>
-      <div>
-        {products.map((product) => (
-          <div
-            className="flex max-w-[300px] flex-col transition-all duration-300 ease-in-out hover:scale-105"
-            key={product.id}
-          >
-            <img
-              src={product.imageUrl}
-              alt={product.name}
-              className="mb-4 h-48 w-full rounded-md object-contain"
-            />
-            <PoppinsText fontSize="16px" className="max-w-[500px]">
-              {product.category}
-            </PoppinsText>
-            <PoppinsText fontSize="16px" className="max-w-[500px]">
-              {product.name}
-            </PoppinsText>
-            <PoppinsText fontSize="16px" className="max-w-[500px]">
-              {product.price}
-            </PoppinsText>
-          </div>
-        ))}
-      </div>
+      {/* products */}
+      <Products />
     </div>
   )
 }
