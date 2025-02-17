2b857903edabc7495cd0320e70ce3fd5b938bc09
enriquesuarezzz <enriquesuarezmartin@gmail.com>
2025-02-17 14:44:28 +0000

    navbarUpdate

--- src/components/molecules/navbar/navbar.tsx
+++ src/components/molecules/navbar/navbar.tsx
@@ -21,17 +21,32 @@ export default async function NavBar() {
       />
       <div className="hidden items-center gap-10 md:flex">
         <Link href="/">
-          <PoppinsText tag="h1" fontSize="16px" style="bold">
+          <PoppinsText
+            tag="h1"
+            fontSize="16px"
+            style="bold"
+            className="relative mx-auto block w-fit text-black after:absolute after:block after:h-[3px] after:w-full after:origin-center after:scale-x-0 after:bg-[#ccb32b] after:transition after:duration-300 after:content-[''] after:hover:scale-x-100"
+          >
             {t('home')}
           </PoppinsText>
         </Link>
         <Link href="/about_us">
-          <PoppinsText tag="h1" fontSize="16px" style="bold">
+          <PoppinsText
+            tag="h1"
+            fontSize="16px"
+            style="bold"
+            className="relative mx-auto block w-fit text-black after:absolute after:block after:h-[3px] after:w-full after:origin-center after:scale-x-0 after:bg-[#ccb32b] after:transition after:duration-300 after:content-[''] after:hover:scale-x-100"
+          >
             {t('about_us')}
           </PoppinsText>
         </Link>
         <Link href="/products">
-          <PoppinsText tag="h1" fontSize="16px" style="bold">
+          <PoppinsText
+            tag="h1"
+            fontSize="16px"
+            style="bold"
+            className="relative mx-auto block w-fit text-black after:absolute after:block after:h-[3px] after:w-full after:origin-center after:scale-x-0 after:bg-[#ccb32b] after:transition after:duration-300 after:content-[''] after:hover:scale-x-100"
+          >
             {t('products')}
           </PoppinsText>
         </Link>
