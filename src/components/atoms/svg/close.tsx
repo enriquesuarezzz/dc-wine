089ee4dbe23053773fffbad79c4b3f2d27ac5702
enriquesuarezzz <enriquesuarezmartin@gmail.com>
2025-02-20 21:30:47 +0000

    closeSVGUpdate

--- src/components/atoms/svg/close.tsx
+++ src/components/atoms/svg/close.tsx
@@ -4,9 +4,10 @@ export default function Close(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="28"
-      height="28"
+      width="22"
+      height="22"
       viewBox="0 0 24 24"
+      {...props}
     >
       <path
         fill="#000000"
