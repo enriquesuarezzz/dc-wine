7dcaa7c8413cdf86dafc6ae218e0db73504885fa
enriquesuarezzz <enriquesuarezmartin@gmail.com>
2025-02-25 08:18:30 +0000

    cookiesModalUpdate

--- src/components/molecules/cookies_modal/cookies_modal.tsx
+++ src/components/molecules/cookies_modal/cookies_modal.tsx
@@ -3,14 +3,24 @@ import { COOKIE_CONSENT, CookieClient } from '@/utils/cookie'
 import { useState } from 'react'
 import { Open_Sans } from 'next/font/google'
 import { PoppinsText } from '@/components/atoms/poppins_text'
+import Cookies from '@/components/atoms/svg/cookies'
 
 export const openSans = Open_Sans({ subsets: ['latin'] })
 
 export interface CookiesModalProps {
   checked: boolean
+  translations: {
+    title: string
+    message: string
+    accept: string
+    reject: string
+  }
 }
 
-export default function CookiesModal({ checked }: CookiesModalProps) {
+export default function CookiesModal({
+  checked,
+  translations,
+}: CookiesModalProps) {
   const [showModal, setShowModal] = useState(!checked)
 
   const handleAcceptCookie = () => {
@@ -22,30 +32,32 @@ export default function CookiesModal({ checked }: CookiesModalProps) {
     CookieClient.deleteCookie(COOKIE_CONSENT)
     setShowModal(false)
   }
+
   return (
     showModal && (
-      <div
-        className={`fixed bottom-0 left-0 z-40 flex h-fit w-fit flex-col justify-between gap-6 rounded-t-3xl bg-black/60 px-6 py-10 font-bold backdrop-blur-sm md:bottom-4 md:left-4 md:rounded-3xl`}
-      >
+      <div className="fixed bottom-0 left-0 z-40 flex h-fit w-fit flex-col justify-between gap-6 rounded-t-3xl bg-gold px-6 py-10 backdrop-blur-sm md:bottom-4 md:left-4 md:rounded-3xl">
         <div className="flex h-fit w-full max-w-[300px] flex-col gap-10">
           {/* Title + description */}
           <div className="flex h-fit w-full flex-col gap-5 text-center">
-            {/* Title */}
-            <PoppinsText
-              fontSize="32px"
-              className="max-w-[500px] text-center text-white"
-            >
-              Cookies
-            </PoppinsText>
+            <div className="flex items-center justify-center gap-4">
+              {/* Title */}
+              <PoppinsText
+                fontSize="32px"
+                style="semibold"
+                className="max-w-[500px] text-center text-black"
+              >
+                {translations.title}
+              </PoppinsText>
+              <Cookies />
+            </div>
             {/* Description */}
             <div className="flex h-fit w-full flex-col gap-4">
               <PoppinsText
                 fontSize="14px"
-                className="max-w-[500px] text-center text-white"
+                style="semibold"
+                className="max-w-[500px] text-center text-black"
               >
-                En nuestro sitio web utilizamos cookies propias y de terceros
-                para finalidades analíticas mediante el análisis del tráfico
-                web.
+                {translations.message}
               </PoppinsText>
             </div>
           </div>
@@ -53,26 +65,26 @@ export default function CookiesModal({ checked }: CookiesModalProps) {
           <div className="flex h-fit w-full flex-row justify-center gap-5">
             {/* Decline */}
             <button
-              className="flex h-fit flex-col items-center justify-center border px-9 py-3 text-white hover:bg-white hover:text-black"
-              onClick={() => handleCancelCookie()}
+              className="flex h-fit flex-col items-center justify-center border border-black px-3 py-2 text-black md:px-9 md:py-3"
+              onClick={handleCancelCookie}
             >
               <PoppinsText
                 fontSize="14px"
                 className="max-w-[500px] text-center"
               >
-                Rechazar
+                {translations.reject}
               </PoppinsText>
             </button>
             {/* Accept */}
             <button
-              className="y flex h-fit flex-col items-center justify-center border bg-black px-9 py-3 text-white hover:bg-white hover:text-black"
-              onClick={() => handleAcceptCookie()}
+              className="flex h-fit flex-col items-center justify-center border border-white bg-black px-3 py-2 text-white md:px-9 md:py-3"
+              onClick={handleAcceptCookie}
             >
               <PoppinsText
                 fontSize="14px"
                 className="max-w-[500px] text-center"
               >
-                aceptar
+                {translations.accept}
               </PoppinsText>
             </button>
           </div>
