257e5abb2ddc44fe202a01edfd8c3f8f780d2f6c
enriquesuarezzz <enriquesuarezmartin@gmail.com>
2025-02-25 08:18:52 +0000

    pageUpdate

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,3 +1,4 @@
+import AgeVerificationPopup from '@/components/molecules/age_verification_pop_up/age_verification_pop_up'
 import Features from '@/components/molecules/features/features'
 import Header from '@/components/molecules/header/header'
 import TopSellingProducts from '@/components/molecules/top_selling_products/top_selling_products'
@@ -13,8 +14,15 @@ export async function generateMetadata() {
 }
 
 export default async function HomePage() {
+  const t = await getTranslations('age_verification_pop_up')
   return (
     <main className="flex min-h-screen flex-col">
+      <AgeVerificationPopup
+        title={t('title')}
+        message={t('message')}
+        accept={t('accept')}
+        exit={t('exit')}
+      />
       <Header />
       <TopSellingProducts />
       <Features />
