f79a8c633e2d335ef5d05a2a2573d5ab220876e3
enriquesuarezzz <enriquesuarezmartin@gmail.com>
2025-03-22 20:00:57 +0000

    productsSlugUpdate

--- src/app/[locale]/products/[id]/page.tsx
+++ src/app/[locale]/products/[id]/page.tsx
@@ -232,7 +232,8 @@ export default function ProductDetails() {
             {product.stock > 0 ? (
               <div className="flex flex-col items-center gap-2">
                 <span className="rounded-full bg-green-100 px-3 py-1 text-green-800">
-                  {locale === 'en' ? 'In Stock' : 'En stock'}
+                  {locale === 'en' ? 'In Stock: ' : 'En stock: '}{' '}
+                  {product.stock}
                 </span>
               </div>
             ) : (
